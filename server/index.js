4d76e982b126918d23b750f621cefc083a953671
jarpi <josep.arpi@gmail.com>
2016-04-24 21:24:46 +0200

    ...

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.ENV.port || 8080; 
+var port = process.env.PORT || 8080; 
 var serverCtrl = require('./lib/model/servers').servers; 
 
 var routes = require('./lib/routes/index')( app, serverCtrl ); 
